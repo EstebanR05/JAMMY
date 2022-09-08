ceb261d7dc323913eb59d280adbe9e95146565bc
esteban <e05072003@gmail.com>
2022-09-07 21:48:30 -0500

    se modifica la ubicacion de las carpetas

--- frontend/shared/services/api.js
+++ frontend/shared/services/api.js
@@ -1,12 +1,11 @@
-
-const API = 'http://localhost/usuarios'
-
 const xhr = new XMLHttpRequest();
-
 function onRequestHandler() {
     console.log(this.response);
 }
-
 xhr.addEventListener('load', onRequestHandler());
 xhr.open('GET', `${API}/?consultar=75`)
 xhr.send()
+
+API = 'http://localhost/usuarios'
+
+fetch().then(response => response.json()).then(json => console.log(json))
