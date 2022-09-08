4227f74d6a795dd7edb3eda7147115d8730726c5
esteban <e05072003@gmail.com>
2022-09-07 22:25:59 -0500

    casi consumiendo api

--- backend/PHP/api.php
+++ backend/PHP/api.php
@@ -6,7 +6,7 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 // Conecta a la base de datos  con usuario, contraseña y nombre de la BD
-$servidor = "localhost"; $usuario = "root"; $contrasenia = ""; $nombreBaseDatos = "usuarios";
+$servidor = "localhost"; $usuario = "root"; $contrasenia = ""; $nombreBaseDatos = "jammy";
 $conexionBD = new mysqli($servidor, $usuario, $contrasenia, $nombreBaseDatos);
 
 
