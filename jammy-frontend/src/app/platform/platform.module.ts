14ffac70da8f3a371befe90cd7f9617c52af36ba
john <john.montano@lumation.co>
2022-09-20 21:19:28 -0500

    principal changes

--- jammy-frontend/src/app/platform/platform.module.ts
+++ jammy-frontend/src/app/platform/platform.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent
+  ],
   imports: [
     CommonModule
   ]
