8b43eb4d57b92f74fd511029749df0c1c4988501
Steiner <juegossteinerherrera123456@gmail.com>
2022-10-07 16:18:42 -0500

    login 1.7/2

--- jammy-frontend/src/app/shared/interfaces/api.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface APIJammy {
-    id:         string;
-    nombre:     string;
-    correo:     string;
-    apellido:   string;
-    contrasena: string;
-}
--- jammy-frontend/src/app/shared/services/login.service.ts
+++ jammy-frontend/src/app/shared/services/login.service.ts
@@ -1,19 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { LoginInterface } from '../interfaces/login.interface';
+import { LoginInterface } from '../interfaces/Login';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
-  API:string = 'http://localhost/usuarios/';
+  ApiUrl:string = 'http://localhost/usuarios/';
+
+  
   constructor( private HttpUsuarios:HttpClient) {
 
    }
 
 
-   VerificarUsuario(datos: LoginInterface):Observable<any> {
-    return this.HttpUsuarios.get(this.API + "?consultar=" + datos)
+   VerificarUsuario(datosUser: LoginInterface):Observable<any> {
+    
+    return this.HttpUsuarios.post<LoginInterface>(`${this.ApiUrl}?consultar=1`, datosUser);
    }
 }
