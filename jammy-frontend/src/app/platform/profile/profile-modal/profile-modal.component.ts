dd47e33ae662cc8ad263ec9f288612a31c81dfc9
john <john.montano@lumation.co>
2022-10-15 20:41:54 -0500

    queda listo los formularios reactivos en el modal

--- jammy-frontend/src/app/platform/profile/profile-modal/profile-modal.component.ts
+++ jammy-frontend/src/app/platform/profile/profile-modal/profile-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from 'src/app/shared/services/user.service';
 
 @Component({
   selector: 'app-profile-modal',
@@ -9,14 +10,23 @@ import { FormGroup } from '@angular/forms';
 export class ProfileModalComponent implements OnInit {
 
   id: any;
-  // form: FormGroup;
+  form!: FormGroup;
 
   @Input() modal: any;
   @Output() closeModal: EventEmitter<any> = new EventEmitter();
 
-  constructor() { }
+  constructor(
+    private _form: FormBuilder,
+    private _data: UserService
+  ) { }
 
   ngOnInit(): void {
+    this.form = this._form.group({
+      nombre: ['', [Validators.required]],
+      info:['', [Validators.required]],
+      hinicial:['', [Validators.required]],
+      hentrega:['', [Validators.required]]
+    });
   }
 
   submit() { }
