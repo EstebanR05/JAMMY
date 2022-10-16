b6488fa7b1b3c15d516aa90ead0f2ff667f67fe5
john <john.montano@lumation.co>
2022-10-15 20:22:24 -0500

    se acomoda totalmente la maquetacion de  profile

--- jammy-frontend/src/app/platform/profile/profile-modal/profile-modal.component.ts
+++ jammy-frontend/src/app/platform/profile/profile-modal/profile-modal.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-modal',
@@ -7,13 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileModalComponent implements OnInit {
 
+  id: any;
+  // form: FormGroup;
+
+  @Input() modal: any;
+  @Output() closeModal: EventEmitter<any> = new EventEmitter();
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  submit(){
-
-  }
+  submit() { }
 
 }
